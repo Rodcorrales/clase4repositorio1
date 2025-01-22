c136d9cfcd3ae18fb0f44b8380f4a25c9660dfda
rodri <rodri@192.168.0.179>
2025-01-21 20:26:45 -0500

    Proyecto Parcial Corrales

--- src/proyecto1_clase4_/Proyecto1_Clase4_.java
+++ src/proyecto1_clase4_/Proyecto1_Clase4_.java
@@ -14,15 +14,17 @@ public class Proyecto1_Clase4_ {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int n1,n2,suma;
+        int n1,n2,n3,suma;
         Scanner lectura = new Scanner(System.in);
         // entrada de datos
-        System.out.print("Ingresar Número 1:");
+        System.out.print("Ingresar Numero 1:");
         n1=lectura.nextInt();
-             System.out.print("Ingresar Número21:");
+             System.out.print("Ingresar Numero 2:");
         n2=lectura.nextInt();
+             System.out.print("Ingresar Numero 3:");
+        n3=lectura.nextInt();
         //proceso de datos
-        suma=n1+n2;
+        suma=n1+n2+n3;
         //salida de datos
         System.out.println("La suma de los numeros es:"+suma);
                
